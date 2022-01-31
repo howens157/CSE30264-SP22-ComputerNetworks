b619d508460f316fc411401bfadbcc20c65fc1f3
Riley Griffith <rgriffith@vailsys.com>
2022-01-31 17:29:27 -0500

    fixed argument error handling

--- server.c
+++ server.c
@@ -51,9 +51,11 @@ int main(int argc, char *argv[])
     int rv;
     char *PORT;
 
-    if(argv != 2)
+    if(argc != 2){
+        printf("Problem with arguments\n");
+        printf("Format is: ./server [PORT]");
         exit(EXIT_FAILURE);
-
+    }
     PORT = argv[1];
 
     memset(&hints, 0, sizeof hints);
@@ -124,19 +126,16 @@ int main(int argc, char *argv[])
             s, sizeof s);
         printf("server: got connection from %s\n", s);
         char buf[100];
-        if (!fork()) { // this is the child process
-            if(recv(sockfd, buf, 99, 0) == -1)
-                perror("receive");
-            close(sockfd);
+        printf("created buffer");
+        if(recv(sockfd, buf, 99, 0) == -1) perror("receive");
+        printf("received from sockfd");
+        close(sockfd);
+        printf("closed sockfd");
+        printf("server received: %s\n", buf);
 
-            printf("server received: %s\n", buf);
-
-            if (send(new_fd, "Hello, world!", 13, 0) == -1)
-                perror("send");
-            close(new_fd);
-            exit(0);
-        }
-        close(new_fd);  // parent doesn't need this
+        if (send(new_fd, "Hello, world!", 13, 0) == -1) perror("send");
+        close(new_fd);
+        exit(0);
     }
 
     return 0;
