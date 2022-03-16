d63852d34353a1b1a63701b3b49554ba72f53caa
Lauren Korbel <lkorbel@student11.cse.nd.edu>
2022-03-16 14:52:48 -0400

    client updates

--- project2/client.py
+++ project2/client.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
 import socket
+import json
 
-HOST = "127.0.0.1"
-PORT = 41100
+f = open('server_info.json')
 
+info = json.load(f)
+
+HOST = info[0]['servername']
+PORT = info[0]['port']
+
+f.close()
+
+print(HOST, PORT)
+
+# read json from file
+f = open('data.json')
+data = json.load(f)
+	
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
 	s.connect((HOST, PORT))
-	s.sendall(b"Hello, world")
-	data = s.recv(1024)
+	data0 = data[0]
+	s.sendall(bytes(str(data0), encoding="utf-8"))
+	print("sent data")
 
-print(f"Received {data!r}")
