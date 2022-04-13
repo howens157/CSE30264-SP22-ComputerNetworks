c8135db93b0da401d2ec92ad1561a867c1e21cf4
rgriffi8 <rgriffi8@nd.edu>
2022-04-12 22:19:00 -0400

    client receive json from server

--- project3/mpwordle/mpwordle.py
+++ project3/mpwordle/mpwordle.py
@@ -35,55 +35,8 @@ def handleCommand(cmdJSON, HOST, PORT):
 		retJSONstr = s.recv(retLen)
 		retJSONstr = retJSONstr.decode()
 		retJSON = json.loads(retJSONstr)
-		# check if the command succesfully executed, if so, output the relevant returned information
-		if retJSON["success"] == True:
-			print(f'Succesfully executed {retJSON["command"]} on {retJSON["calendar"]}')
-			if retJSON["command"] == "add":
-				print(f'Added event with identifier: {retJSON["identifier"]}')
-			elif retJSON["command"] == "remove":
-				print(f'Removed event with identifier: {retJSON["identifier"]}')
-			elif retJSON["command"] == "update":
-				print(f'Updated event with identifier: {retJSON["identifier"]}')
-			elif retJSON["command"] == "get":
-				print("All events on given day:")
-				for event in retJSON["data"]:
-					event = json.loads(event)
-					print(f'Event ID: {event["id"]}     Date: {event["date"]}     Time: {event["time"]}     Duration(minutes): {event["duration"]}     Name: {event["name"]}', end="")
-					if event["description"] != "":
-						print(f'     Description: {event["description"]}', end="")
-					if event["location"] != "":
-						print(f'     Location: {event["location"]}', end="")
-					print("")
-			elif retJSON["command"] == "getrange":
-				output_d = {}
-				for event in retJSON["data"]:
-					eventJSON = json.loads(event)
-					date = datetime.strptime(eventJSON["date"], '%m%d%y')
-					if date not in output_d:
-						output_d[date] = [eventJSON]
-					else:
-						output_d[date].append(eventJSON)
-				for key in sorted(output_d.keys()):
-					print("Date: " + key.strftime("%B %d, %Y"))
-					for event in output_d[key]:
-						print(f'Event ID: {event["id"]}     Time: {event["time"]}     Duration(minutes): {event["duration"]}     Name: {event["name"]}', end="")
-						if event["description"] != "":
-							print(f'     Description: {eventJSON["description"]}', end="")
-						if event["location"] != "":
-							print(f'     Location: {eventJSON["location"]}', end="")
-						print("")
-					print("\n")
-			# if somehow an invalid command was received back from the server
-			else:
-				print(f'Received unknown command from server: {retJSON["command"]}')
-		# otherwise, if the command failed, output an error message and the error provided by the server
-		elif retJSON["success"] == False:
-			print(f'Failed to execute {retJSON["command"]} on {retJSON["calendar"]}')
-			print(f'Error: {retJSON["error"]}')
-		else:
-			print("Error receiving command.")
-			print("Received:")
-			print(retJSONstr)
+
+		print(retJSON)
 
 def error():
 	print('Usage: ./mpwordle -name X -server X -port X')
